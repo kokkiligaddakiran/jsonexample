837c2da2cc4f716b345485350043bf0e673531f9
ambika <ambikadr08@gmail.com>
2019-09-11 18:47:13 +0530

    Initial Commit

---
+++ jsonData.js
@@ -0,0 +1,48 @@
+var emp=
+    [
+        {
+            "name":"Siri",
+            "id":"ms057",
+            "age":20,
+            "place":"Bangalore"
+        },
+
+        {
+            "name":"Aparna",
+            "id":"ms058",
+            "age":21,
+            "place":"Mysore"
+        },
+
+        {
+            "name":"Mythri",
+            "id":"ms059",
+            "age":22,
+            "place":"Mangalore"
+        },
+
+        {
+            "name":"Ambika",
+            "id":"ms060",
+            "age":23,
+            "place":"Mandya"
+        },
+        {
+            "name":"Kiran",
+            "id":"ms061",
+            "age":24,
+            "place":"Hasan"
+        }
+    ]
+/*console.log(emp[0].name);*/
+/*var b = JSON.parse(emp[0].name);
+console.log(b);*/
+/*
+var firstEmp="name: "+ emp[0].name+ " age: "+ emp[0].age+ " id: "+ emp[0].id+ " place: "+ emp[0].place;
+var secondEmp="name: "+ emp[1].name+ " age: "+ emp[1].age+ " id: "+ emp[1].id+ " place: "+ emp[1].place;
+var thirdEmp="name: "+ emp[2].name+ " age: "+ emp[2].age+ " id: "+ emp[2].id+ " place: "+ emp[2].place;
+var fourhEmp="name: "+ emp[3].name+ " age: "+ emp[3].age+ " id: "+ emp[3].id+ " place: "+ emp[3].place;
+document.writeln(firstEmp);
+document.writeln(secondEmp);
+document.writeln(thirdEmp);
+*/
